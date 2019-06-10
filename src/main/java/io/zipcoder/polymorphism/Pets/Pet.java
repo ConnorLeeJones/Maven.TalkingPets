2559ff66949c0312be91011ecb700a32c3308021
Connor Jones <ConnorLeeJones@gmail.com>
2019-06-10 19:25:04 -0400

    Done

--- src/main/java/io/zipcoder/polymorphism/Pets/Pet.java
+++ src/main/java/io/zipcoder/polymorphism/Pets/Pet.java
@@ -1,11 +1,12 @@
 package io.zipcoder.polymorphism.Pets;
+import java.util.Comparator;
 
-public abstract class Pet {
+public abstract class Pet implements Comparable<Pet> {
     String name;
     String animal;
 
 
-    public Pet(){
+    public Pet() {
         this.name = "";
         this.animal = "";
     }
@@ -31,8 +32,39 @@ public abstract class Pet {
         this.animal = animal;
     }
 
-    public String speak(){
+    public String speak() {
         return "I am pet :)";
+    }
+
+
+    public class nameCompare implements Comparator<Pet>{
+
+        @Override
+        public int compare(Pet o1, Pet o2) {
+            int nameCompare = o2.name.compareTo(o1.name);
+            int typeCompare = o2.animal.compareTo(o1.animal);
+
+            if (nameCompare == 0) {
+                return typeCompare;
+            } else {
+                return nameCompare;
+            }
+        }
+    }
+
+    public class typeCompare implements Comparator<Pet>{
+
+        @Override
+        public int compare(Pet o1, Pet o2) {
+            int nameCompare = o2.name.compareTo(o1.name);
+            int typeCompare = o2.animal.compareTo(o1.animal);
+
+            if (typeCompare == 0) {
+                return nameCompare;
+            } else {
+                return typeCompare;
+            }
+        }
     }
 }
 
